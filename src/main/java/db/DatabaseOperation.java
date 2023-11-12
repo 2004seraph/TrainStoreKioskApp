419f6b1eadd0f4810f35f084f6d62202a7217f1d
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2023-11-12 21:18:10 +0000

    Fixed project merge

--- src/main/java/db/DatabaseOperation.java
+++ src/main/java/db/DatabaseOperation.java
@@ -49,45 +49,6 @@ public final class DatabaseOperation {
         db = conn;
     }
 
-    
-    /**
-     * Inserts a new address into the database
-     * @param address A non-null address that doesn't have any null fields either
-     * @return Whether the insertion was successful or failed due to someone already having that house address
-     * @throws SQLException
-     */
-    public static Boolean CreateAddress(Address address) throws SQLException {
-        // Check if this address already exists using the primary key houseNumber and postCode
-        
-        try (PreparedStatement s = db.prepareStatement("SELECT * FROM Address WHERE houseNumber=? AND postCode=?")){
-            Object [] fields = address.GetFields().toArray();
-
-            s.setString(1, fields[0].toString());
-            s.setString(2, fields[3].toString());
-
-            ResultSet res = s.executeQuery();
-            if(!res.next()){
-                // Create a new address to the database
-                try (PreparedStatement r = db.prepareStatement("INSERT INTO Address VALUES (?,?,?,?)")){
-                    r.setString(1, fields[0].toString());
-                    r.setString(2, fields[1].toString());
-                    r.setString(3, fields[2].toString());
-                    r.setString(4, fields[3].toString());
-                    r.executeUpdate();
-                } catch (SQLException e) {
-                    DatabaseBridge.databaseError("Failed to insert new address");
-                    throw e;
-                }
-            }
-        } catch (SQLException e) {
-            DatabaseBridge.databaseError("Failed to insert new address");
-            throw e;
-        }
-        return true;
-    }
-
-
-
     /**
      * Inserts a new address into the database
      * @param address A non-null address that doesn't have any null fields either
