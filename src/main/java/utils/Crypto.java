1cccf08f85b2de58ad91d7d86ab1e0d1e188012d
= <ldormon2@sheffield.ac.uk>
2023-11-16 18:31:37 +0000

    feat(Utils/Cypto): encryption wrappers

--- src/main/java/utils/Crypto.java
+++ src/main/java/utils/Crypto.java
@@ -2,10 +2,17 @@ package utils;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
 
 /**
  * Static utility class for abstracting hash algorithm implementation
@@ -38,6 +45,62 @@ public class Crypto {
         }
 
         return null;
+    }
+
+    public static String encryptString(String input, byte[] key) throws
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            UnsupportedEncodingException, NoSuchPaddingException,
+            NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] iv = new byte[16];
+        secureRandom.nextBytes(iv);
+        IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivspec);
+
+        byte[] cipherText = cipher.doFinal(input.getBytes("UTF-8"));
+        byte[] encryptedData = new byte[iv.length + cipherText.length];
+        System.arraycopy(iv, 0, encryptedData, 0, iv.length);
+
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    public static String decryptString(String input, byte[] key) throws
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            UnsupportedEncodingException, NoSuchPaddingException,
+            NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException {
+        byte[] encryptedData = Base64.getDecoder().decode(input);
+        byte[] iv = new byte[16];
+        System.arraycopy(encryptedData, 0, iv, 0, iv.length);
+        IvParameterSpec ivspec = new IvParameterSpec(iv);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivspec);
+
+        byte[] cipherText = new byte[encryptedData.length - 16];
+        System.arraycopy(encryptedData, 16, cipherText, 0, cipherText.length);
+
+        byte[] decryptedText = cipher.doFinal(cipherText);
+        return new String(decryptedText, "UTF-8");
+    }
+
+    public static void main(String[] args) {
+        byte[] key = deriveEncryptionKey("password123");
+        System.out.println("Key: " + Base64.getEncoder().encodeToString(key));
+
+        try {
+            String encrypted = encryptString("Hello World", key);
+            System.out.println("Encrypted: " + encrypted);
+
+            String decrypted = decryptString(encrypted, key);
+            System.out.println("Decrypted: " + decrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
