d2bad0ca9689f4ca2a6726c7ebb4f7c3c183dc33
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2023-11-26 21:22:10 +0000

    [Sync]

--- src/main/java/gui/person/Profile.java
+++ src/main/java/gui/person/Profile.java
@@ -95,61 +95,42 @@ public class Profile extends JPanel{
                 postCode.setText(person.getAddress().getPostcode());
             }
 
-            if (person.getBankDetail() != null) {
 //                Add bank details to the profile
 //                Add bank details title
-                JLabel bankDetailsTitle = new JLabel("<html><h2>Bank Details</h2></html>");
-                gbc.gridx = 0;
-                gbc.gridy = 9;
-                gbc.gridwidth = 2;
-                gbc.fill = GridBagConstraints.HORIZONTAL;
-                gbc.anchor = GridBagConstraints.CENTER;
-                add(bankDetailsTitle, gbc);
+            JLabel bankDetailsTitle = new JLabel("<html><h2>Bank Details</h2></html>");
+            gbc.gridx = 0;
+            gbc.gridy = 9;
+            gbc.gridwidth = 2;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.anchor = GridBagConstraints.CENTER;
+            add(bankDetailsTitle, gbc);
 
-                gbc.fill = GridBagConstraints.NONE;
-                gbc.anchor = GridBagConstraints.WEST;
-                //    Bank Details
-                JLabel cardNumberLabel = new JLabel("Card Number:");
-                cardNumber = new JTextField(30);
-                addField(gbc, cardNumberLabel, cardNumber, 10);
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.anchor = GridBagConstraints.WEST;
+            //    Bank Details
+            JLabel cardNumberLabel = new JLabel("Card Number:");
+            cardNumber = new JTextField(30);
+            addField(gbc, cardNumberLabel, cardNumber, 10);
 
-                JLabel expiryDateLabel = new JLabel("Expiry Date:");
-                expiryDate = new JTextField(30);
-                addField(gbc, expiryDateLabel, expiryDate, 11);
+            JLabel expiryDateLabel = new JLabel("Expiry Date:");
+            expiryDate = new JTextField(30);
+            addField(gbc, expiryDateLabel, expiryDate, 11);
 
-                JLabel securityCodeLabel = new JLabel("Security Code:");
-                securityCode = new JTextField(30);
-                addField(gbc, securityCodeLabel, securityCode, 12);
+            JLabel securityCodeLabel = new JLabel("Security Code:");
+            securityCode = new JTextField(30);
+            addField(gbc, securityCodeLabel, securityCode, 12);
 
-                cardNumber.setText(person.getBankDetail().getCardNumber());
-                expiryDate.setText(person.getBankDetail().getExpiryDate().toString());
-                securityCode.setText(person.getBankDetail().getSecurityCode());
-
-//                For the update button
-                gbc.gridx = 0;
-                gbc.gridy = 13;
-                gbc.gridwidth = 2;
-                gbc.fill = GridBagConstraints.NONE;
-                gbc.anchor = GridBagConstraints.CENTER;
-            } else {
-                gbc.gridx = 1;
-                gbc.gridy = 10;
-                JButton addBankDetailsButton = new JButton("ADD BANK DETAILS");
-                add(addBankDetailsButton, gbc);
-//                For the update button
-                gbc.gridx = 0;
-                gbc.gridy = 10;
-
-                addBankDetailsButton.addActionListener(e -> {
-                    getBankDetailsFromUser();
-                });
-            }
-            //    Buttons
-            JButton updateButton = new JButton("UPDATE");
-            add(updateButton, gbc);
-            updateButton.addActionListener(e -> {
-                updateDetails();
-            });
+        if (person.getBankDetail() != null) {
+            cardNumber.setText(person.getBankDetail().getCardNumber());
+            expiryDate.setText(person.getBankDetail().getExpiryDate().toString());
+            securityCode.setText(person.getBankDetail().getSecurityCode());
+        }
+        gbc.gridy++;
+        JButton updateButton = new JButton("UPDATE");
+        add(updateButton, gbc);
+        updateButton.addActionListener(e -> {
+            updateDetails();
+        });
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -202,12 +183,14 @@ public class Profile extends JPanel{
         try {
             db.openConnection();
 
-            PreparedStatement emailLookup = db.prepareStatement("SELECT email FROM Person WHERE email=?");
+            PreparedStatement emailLookup = db.prepareStatement("SELECT PersonId FROM Person WHERE email=?");
             emailLookup.setString(1, emailInput);
             ResultSet res = emailLookup.executeQuery();
-            if (res.next()) {
+            if (res.next() && res.getInt(1) != AppContext.getCurrentUser().getId()) {
                 throw new IllegalArgumentException("Another user already exists with that email address");
             }
+
+            System.out.println(Date.valueOf(expiryInput));
 
             BankDetail newBankDetails = BankDetail.createPaymentInfo(cardNumberInput, Date.valueOf(expiryInput), securityInput);
             AppContext.getCurrentUser().addNewBankDetails(newBankDetails);
@@ -220,123 +203,5 @@ public class Profile extends JPanel{
         } finally {
             db.closeConnection();
         }
-
-    }
-
-    /**
-     * Creates a new JFrame as a pop up that allows the user to enter their bank details
-     */
-    public void getBankDetailsFromUser() {
-        // Create a new JFrame
-        JDialog frame = new JDialog(AppContext.getWindow());
-        frame.setTitle("Bank Details");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.insets = new Insets(10, 10, 10, 10);
-
-        // Create the labels
-        JLabel cardNumberLabel = new JLabel("Card Number");
-        JLabel expiryDateLabel = new JLabel("Expiry Date (yyyy-mm-dd)");
-        JLabel securityCodeLabel = new JLabel("Security Code");
-
-        // Create the text fields
-        JTextField cardNumber = new JTextField(25);
-        JTextField expiryDate = new JTextField(25);
-        JTextField securityCode = new JTextField(25);
-
-        // Create the button
-        JButton submitButton = new JButton("Submit");
-
-//        Add the title
-        JLabel title = new JLabel("<html><h1>Bank Details</h1></html>");
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.anchor = GridBagConstraints.CENTER;
-        frame.add(title, gbc);
-
-        // Add the labels and text fields to the JFrame
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.anchor = GridBagConstraints.WEST;
-
-        gbc.gridwidth = 1;
-
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        frame.add(cardNumberLabel, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        frame.add(cardNumber, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        frame.add(expiryDateLabel, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        frame.add(expiryDate, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        frame.add(securityCodeLabel, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        frame.add(securityCode, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.anchor = GridBagConstraints.CENTER;
-        frame.add(submitButton, gbc);
-
-        frame.setVisible(true);
-
-        // When clicking submitButton, close this window and open Register window
-        submitButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String cardNumberInput = cardNumber.getText();
-                String expiryDateInput = expiryDate.getText();
-                String securityCodeInput = securityCode.getText();
-
-                Date expiryDate = Date.valueOf(expiryDateInput);
-
-                try{
-                   BankDetail.validateBankDetails(cardNumberInput, expiryDateInput, securityCodeInput);
-                } catch (BankDetail.InvalidBankDetailsException exception) {
-                    JOptionPane.showMessageDialog(AppContext.getWindow(), exception.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                }
-
-                DatabaseBridge db = DatabaseBridge.instance();
-                try{
-                    db.openConnection();
-                    bankDetail = BankDetail.createPaymentInfo(cardNumberInput, expiryDate, securityCodeInput);
-//                    Add the bank details to the user
-                    AppContext.getCurrentUser().addNewBankDetails(bankDetail);
-                    JOptionPane.showMessageDialog(AppContext.getWindow(), "Bank Details Added");
-                } catch (Exception exception) {
-                    JOptionPane.showMessageDialog(AppContext.getWindow(), exception.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                } finally {
-                    db.closeConnection();
-                    frame.dispose();
-                }
-
-                // reload the current state
-                try {
-                    db.openConnection();
-                    AppContext.setCurrentUser(Person.getPersonByID(AppContext.getCurrentUser().getId()));
-                } catch (Exception exception) {
-                    throw new RuntimeException(exception);
-                } finally {
-                    db.closeConnection();
-                }
-
-                revalidate();
-                repaint();
-            }
-        });
     }
 }
