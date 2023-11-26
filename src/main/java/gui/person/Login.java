f5742b2c5db936153a71d84d9efeefd3204d978d
Sam Taseff <sgttaseff1@sheffield.ac.uk>
2023-11-26 21:28:32 +0000

    [Fix] Removed duplicated database operation

--- src/main/java/gui/person/Login.java
+++ src/main/java/gui/person/Login.java
@@ -34,13 +34,13 @@ public class Login extends JPanel implements TabbedGUIContainer.TabPanel {
 
         JLabel emailLabel = new JLabel("Email");
         email = new JTextField (5);
-        email.setText("najaaznabhan@gmail.com");
-//        email.setText("sam@sheffield.ac.uk");
+//        email.setText("najaaznabhan@gmail.com");
+        email.setText("sam@sheffield.ac.uk");
 
         JLabel passwordLabel = new JLabel("Password");
         password = new JPasswordField (5);
-        password.setText("Naajid12345!");
-//        password.setText("password123");
+//        password.setText("Naajid12345!");
+        password.setText("password123");
 
         JButton loginButton = new JButton("Login");
         JLabel registerLabel = new JLabel("<html><u><font color='blue'>Register Now</font></u></html>");
